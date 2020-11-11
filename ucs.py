b45e4f730d4c1a0e1d31c2cb189f89064dc6b055
yuxiang <yuxiang.zhang@mail.concordia.ca>
2020-11-11 00:15:59 -0500

    finish successor function; add successor unit test

--- ucs.py
+++ ucs.py
@@ -10,16 +10,19 @@ class Puzzle8:
     goal1_hash = hash((1, 2, 3, 4, 5, 6, 7, 0))
     goal2_hash = hash((1, 3, 5, 7, 2, 4, 6, 0))
 
-    dict_conf = {goal1_hash:[1, 2, 3, 4, 5, 6, 7, 0], goal2_hash:[1, 3, 5, 7, 2, 4, 6, 0]}
+    dict_conf = {goal1_hash:(1, 2, 3, 4, 5, 6, 7, 0), goal2_hash:(1, 3, 5, 7, 2, 4, 6, 0)}
 
-    def __init__(self, start_config):
-        assert(len(start_config) == 8)
+    def __init__(self, start_config:iter):
+        if len(start_config) != 8:
+            raise Exception('Bad length for an 8-puzzle')
         self.config = list(start_config)
         self.open_list = []
         self.closed_list = {}
 
     def __hash__(self):
-        return hash(tuple(self.config))
+        key = hash(tuple(self.config))
+        Puzzle8.dict_conf[key] = tuple(self.config)
+        return key
 
     def __eq__(self, other):
         if isinstance(other, Puzzle8):
@@ -29,69 +32,71 @@ class Puzzle8:
     def is_goal(self):
         return self.__hash__() == Puzzle8.goal1_hash or self.__hash__() == Puzzle8.goal2_hash
 
-    def get_next_conf(self, blank, move):
+    def hash_next_conf(self, blank, tile):
         conf = list(self.config)
-        conf[blank], conf[move] = conf[move], conf[blank]
+        conf[blank], conf[tile] = conf[tile], conf[blank]
         key = hash(tuple(conf))
-        if key not in self.dict_conf:
-            self.dict_conf[key] = conf
+        if key not in Puzzle8.dict_conf:
+            Puzzle8.dict_conf[key] = tuple(conf)
         return key
 
-    def move(self, move_type, blank):
-        cost = 0
-        swap_pos = []
+    def move(self, blank):
+        moves = {1:[], 2:[], 3:[]}
         ans = {}
-        if move_type == self.Move.Horizontal:
-            cost = 1
-            if blank not in [0, 4]:
-                swap_pos.append(blank - 1)
-            if blank not in [3, 7]:
-                swap_pos.append(blank + 1)
-        elif move_type == self.Move.Vertical:
-            cost = 1
-            swap_pos.append(blank + 4 if blank < 4 else blank - 4)
-        elif move_type == self.Move.Wrapping:
+
+        # Regular moves
+        # Horizontal
+        cost = 1
+        if blank not in [0, 4]:
+            moves[cost].append(blank - 1)
+        if blank not in [3, 7]:
+            moves[cost].append(blank + 1)
+
+        # Vertical
+        moves[cost].append(blank + 4 if blank < 4 else blank - 4)
+
+        # Corner moves
+        if self.config.index(0) in [0, 3, 4, 7]:
+            # Wrapping
             cost = 2
             if blank in [0, 4]:
-                swap_pos.append(blank + 3)
+                moves[cost].append(blank + 3)
             else:
-                swap_pos.append(blank - 3)
-        elif move_type == self.Move.Diagonal:
+                moves[cost].append(blank - 3)
+            # Diagonal
             cost = 3
             if blank == 0:
-                swap_pos.extend([5, 7])
+                moves[cost].extend([5, 7])
             elif blank == 3:
-                swap_pos.extend([4,6])
+                moves[cost].extend([4, 6])
             elif blank == 4:
-                swap_pos.extend([1,3])
+                moves[cost].extend([1, 3])
             elif blank == 7:
-                swap_pos.extend([0, 2])
+                moves[cost].extend([0, 2])
 
-        for i in swap_pos:
-            next_conf_key = self.get_next_conf(blank, i)
-            ans[cost] = next_conf_key
+        for cost, tiles in moves.items():
+            for tile in tiles:
+                ans[self.hash_next_conf(blank, tile)] = cost
+
         return ans
 
     def successor(self):
         blank = self.config.index(0)
-        if self.config.index(0) in [0, 3, 4, 7]:
-            # corner moves
-            next_confs = self.move(self.Move.Wrapping, blank)
-            for next_cost, next_conf in next_confs.items():
-                if next_conf not in self.closed_list:
-                    heapq.heappush(self.open_list)
-                    # for i, (cost, conf_pair) in enumerate(self.open_list): #check if already in open_list
-                    #     if next_conf == conf_pair[0]:
-                    #         if next_cost < cost: # if cost is less, update cost
-                    #             self.open_list[i] = (next_cost, conf_pair)
-                    #             heapq.heapify(self.open_list)
-                    #         break
-            self.move(self.Move.Diagonal, blank)
-            pass
-        self.move()
-        # regular moves
+        successors = self.move(blank)
 
-        pass
+        for new_conf_hash, cost in successors.items():
+            if new_conf_hash not in self.closed_list:
+                heapq.heappush(self.open_list, (cost, tuple((new_conf_hash, self.__hash__()))))
+                #check if already in open_list
+                # for i, (cost, conf_pair) in enumerate(self.open_list):
+                #     if next_conf == conf_pair[0]:
+                #         if next_cost < cost: # if cost is less, update cost
+                #             self.open_list[i] = (next_cost, conf_pair)
+                #             heapq.heapify(self.open_list)
+                #         break
+
+    def __str__(self):
+        return ' '.join(map(str, self.config[:4])) + '\n' + ' '.join(map(str, self.config[4:]))
 
 
 class TestPuzzle(unittest.TestCase):
@@ -100,7 +105,17 @@ class TestPuzzle(unittest.TestCase):
         self.assertTrue(Puzzle8([1, 3, 5, 7, 2, 4, 6, 0]).is_goal())
         self.assertFalse(Puzzle8([4, 2, 3, 1, 5, 6, 7, 0]).is_goal())
 
-    pass
+    def test_successor(self):
+        puzzle = Puzzle8([4, 2, 3, 1, 5, 6, 7, 0])
+        puzzle.successor()
+        initial_hash = hash((4, 2, 3, 1, 5, 6, 7, 0))
+        successors = [(1, (hash((4, 2, 3, 0, 5, 6, 7, 1)), initial_hash)),
+                      (1, (hash((4, 2, 3, 1, 5, 6, 0, 7)), initial_hash)),
+                      (2, (hash((4, 2, 3, 1, 0, 6, 7, 5)), initial_hash)),
+                      (3, (hash((0, 2, 3, 1, 5, 6, 7, 4)), initial_hash)),
+                      (3, (hash((4, 2, 0, 1, 5, 6, 7, 3)), initial_hash))]
+        while puzzle.open_list:
+            self.assertEqual(heapq.heappop(successors), heapq.heappop(puzzle.open_list))
 
 # open_list = PriorityQueue()
 # open_list.put()
@@ -117,6 +132,8 @@ class TestPuzzle(unittest.TestCase):
 # for i in range(len(open_list)):
 #     print(heapq.heappop(open_list))
 # print(hash(((1),(2))))
+
+
 
 if __name__ == '__main__':
     unittest.main()
