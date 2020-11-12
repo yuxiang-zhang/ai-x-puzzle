bccce94df2e31e7dec87815f0c77abbb0b6ed4f5
yuxiang <yuxiang.zhang@mail.concordia.ca>
2020-11-11 20:37:03 -0500

    update test class

---
+++ test.py
@@ -0,0 +1,28 @@
+import heapq
+import unittest
+import puzzle
+import searchstrat
+from state import State
+
+
+class TestPuzzle(unittest.TestCase):
+
+    def test_goal(self):
+        self.assertTrue(puzzle.Puzzle8([1, 3, 5, 7, 2, 4, 6, 0], searchstrat.UniformCost()).is_goal())
+        self.assertFalse(puzzle.Puzzle8([4, 2, 3, 1, 5, 6, 7, 0], searchstrat.UniformCost()).is_goal())
+
+    def test_successor(self):
+        game = puzzle.Puzzle8([4, 2, 3, 1, 5, 6, 7, 0], searchstrat.UniformCost())
+        game.search()
+        init_state = State((4, 2, 3, 1, 5, 6, 7, 0))
+        successors = [(1, (State((4, 2, 3, 0, 5, 6, 7, 1)), init_state)),
+                      (1, (State((4, 2, 3, 1, 5, 6, 0, 7)), init_state)),
+                      (2, (State((4, 2, 3, 1, 0, 6, 7, 5)), init_state)),
+                      (3, (State((0, 2, 3, 1, 5, 6, 7, 4)), init_state)),
+                      (3, (State((4, 2, 0, 1, 5, 6, 7, 3)), init_state))]
+        while successors:
+            self.assertEqual(heapq.heappop(successors), game.search_strat.get_best_next())
+
+
+if __name__ == '__main__':
+    unittest.main()
