0c5ab8546037c386c4e8906115dc4efbd729693f
yuxiang <yuxiang.zhang@mail.concordia.ca>
2020-11-13 13:04:35 -0500

    use distinct loggers for distinct files

--- searchstrat.py
+++ searchstrat.py
@@ -78,9 +78,9 @@ class UniformCost(SearchStrategy, ABC):
         super().__init__(h_func, game)
         if not logging_disabled:
             filename = 'out/{}_{}_'.format(puzzle_num, str(self))
-            self._search_logger = logging.getLogger(str(self)+'.search')
+            self._search_logger = logging.getLogger('.'.join(map(str, ['search', self, puzzle_num])))
             self._search_logger.addHandler(logging.FileHandler(filename + 'search.txt', 'w'))
-            self._sol_logger = logging.getLogger(str(self)+'.sol')
+            self._sol_logger = logging.getLogger('.'.join(map(str, ['sol', self, puzzle_num])))
             self._sol_logger.addHandler(logging.FileHandler(filename + 'solution.txt', 'w'))
 
     def __str__(self):
@@ -100,9 +100,9 @@ class GBFS(SearchStrategy, ABC):
         super().__init__(h_func, game)
         if not logging_disabled:
             filename = 'out/{}_{}_'.format(puzzle_num, str(self))
-            self._search_logger = logging.getLogger(str(self)+'.search')
+            self._search_logger = logging.getLogger('.'.join(map(str, ['search', self, puzzle_num])))
             self._search_logger.addHandler(logging.FileHandler(filename + 'search.txt', 'w'))
-            self._sol_logger = logging.getLogger(str(self)+'.sol')
+            self._sol_logger = logging.getLogger('.'.join(map(str, ['sol', self, puzzle_num])))
             self._sol_logger.addHandler(logging.FileHandler(filename + 'solution.txt', 'w'))
 
     def __str__(self):
@@ -120,9 +120,9 @@ class AStar(SearchStrategy, ABC):
         super().__init__(h_func, game)
         if not logging_disabled:
             filename = 'out/{}_{}_'.format(puzzle_num, str(self))
-            self._search_logger = logging.getLogger(str(self)+'.search')
+            self._search_logger = logging.getLogger('.'.join(map(str, ['search', self, puzzle_num])))
             self._search_logger.addHandler(logging.FileHandler(filename + 'search.txt', 'w'))
-            self._sol_logger = logging.getLogger(str(self)+'.sol')
+            self._sol_logger = logging.getLogger('.'.join(map(str, ['sol', self, puzzle_num])))
             self._sol_logger.addHandler(logging.FileHandler(filename + 'solution.txt', 'w'))
 
     def __str__(self):
