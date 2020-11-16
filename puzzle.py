1fc3d0be00574fde8e8408863e6c74713f38d70e
yuxiang <yuxiang.zhang@mail.concordia.ca>
2020-11-16 15:42:50 -0500

    add scaled puzzle

--- puzzle.py
+++ puzzle.py
@@ -1,4 +1,5 @@
-from state import State
+from state import State, State2D
+import numpy as np
 
 class Puzzle8:
     goal1 = State((1, 2, 3, 4, 5, 6, 7, 0))
@@ -54,5 +55,81 @@ class Puzzle8:
                                         self._state.path_cost + move_cost,
                                         self._state,
                                         self._state.config[tile]))
+
+        return successors
+
+class Puzzle:
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, state):
+        self._state = state
+
+    def __init__(self, init_config: tuple, shape, goals: []):
+        if not goals:
+            raise Exception('No goal was defined. ')
+        # initial state
+        self._state = State2D(np.array(init_config).reshape(shape), 0)
+        self._goals = [hash(np.array(goal).tobytes()) for goal in goals]
+
+    def is_goal(self):
+        return hash(self._state) in self._goals
+
+    def gen_config(self, blank_row, blank_col, tile_row, tile_col):
+        conf = self._state.config.copy()
+        conf[blank_row, blank_col], conf[tile_row, tile_col] = conf[tile_row, tile_col], conf[blank_row, blank_col]
+        return conf
+
+    def successor(self):
+        blank_index = np.where(self._state.config == 0)
+        blank_index = blank_index[0][0], blank_index[1][0]
+        moves = {1:[], 2:[], 3:[]}
+
+        nrows, ncols = self._state.config.shape
+
+        # Vertical or Wrapping
+        if blank_index[0] != 0: #not first row
+            moves[1].append((blank_index[0] - 1, blank_index[1]))
+        else:
+            moves[2].append((nrows - 1, blank_index[1]))
+        if blank_index[0] != nrows - 1: #not last row
+            moves[1].append((blank_index[0] + 1, blank_index[1]))
+        else:
+            moves[2].append((0, blank_index[1]))
+
+        # Horizontal or Wrapping
+        if blank_index[1] != ncols - 1: #not last col
+            moves[1].append((blank_index[0], blank_index[1] + 1))
+        else:
+            moves[2].append((blank_index[0], 0))
+        if blank_index[1] != 0: #not first col
+            moves[1].append((blank_index[0], blank_index[1] - 1))
+        else:
+            moves[2].append((blank_index[0], ncols - 1))
+
+        # Corner moves
+        if blank_index[0] == 0 and blank_index[1] == 0:
+            moves[3].append((nrows - 1, ncols - 1))
+            moves[3].append((1, 1))
+        elif blank_index[0] == nrows - 1 and blank_index[1] == 0:
+            moves[3].append((0, ncols - 1))
+            moves[3].append((nrows - 2, 1))
+        elif blank_index[0] == 0 and blank_index[1] == ncols - 1:
+            moves[3].append((nrows - 1, 0))
+            moves[3].append((1, ncols - 2))
+        else:
+            moves[3].append((0, 0))
+            moves[3].append((nrows - 2, ncols - 2))
+
+        successors = []
+        for move_cost, tiles in moves.items():
+            for tile_index in tiles:
+                successors.append(State2D(self.gen_config(*blank_index, *tile_index),
+                                        self._state.path_cost + move_cost,
+                                        self._state,
+                                        self._state.config[tile_index[0], tile_index[1]]))
 
         return successors
