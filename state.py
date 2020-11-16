6fe2fc84bb1209eb616628decb624415dc8403d3
yuxiang <yuxiang.zhang@mail.concordia.ca>
2020-11-16 13:44:52 -0500

    add State2D

--- state.py
+++ state.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 class State:
     MAX_MOVES = 31
 
@@ -46,3 +49,49 @@ class State:
 
     def __str__(self):
         return ' '.join(map(str, self.config))
+
+class State2D:
+    MAX_MOVES = 31
+
+    @property
+    def config(self):
+        return self._config
+
+    @property
+    def from_state(self):
+        return self._from_state
+
+    @property
+    def path_cost(self):
+        return self._path_cost
+
+    @property
+    def last_moved_tile(self):
+        return self._last_moved_tile
+
+    @path_cost.setter
+    def path_cost(self, cost):
+        self._path_cost = cost
+
+    def __init__(self, init_config: [[]], cost=MAX_MOVES, from_state=None, last_moved_tile=0):
+        self._config = init_config
+        self._path_cost = cost
+        self._from_state = from_state
+        self._last_moved_tile = last_moved_tile
+
+    def __hash__(self):
+        val = tuple(tuple(x) for x in self._config)
+        return hash(val)
+
+    def __eq__(self, other):
+        if isinstance(other, State2D):
+            return hash(self) == hash(other)
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, State2D):
+            return hash(self) < hash(other)
+        return NotImplemented
+
+    def __str__(self):
+        return ' '.join(map(str, list(itertools.chain.from_iterable(self._config))))
